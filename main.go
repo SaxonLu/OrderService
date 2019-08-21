19ec8047991d731251e19c4ae4ac22350170f3b6
LA <la.lu@frog-jump.com>
2019-08-21 14:20:56 +0800

    update 測試post方法

--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	// "encoding/json"
-	// "net/http"
-	// "github.com/gorilla/mux"
-
+	"encoding/json"
+	"net/http"
 	"log"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/gorilla/mux"
 )
-
-// import (
-// 	_ "github.com/mattn/go-adodb"
-// )
 
 type Menu struct {
 	Menu_ID int `db:"Menu_ID"`
@@ -21,41 +16,52 @@ type Menu struct {
 	OnWork  string `db:"OnWork"`
 }
 
-// func MenuPage(rw http.ResponseWriter, request *http.Request) {
+func MenuPage(rw http.ResponseWriter, request *http.Request) {
 
-// 	u := &Menu{
-// 		Meal:  "Steak",
-// 		Price: 199,
-// 	}
-// 	b, err := json.Marshal(u)
-
-// 	if err != nil {
-// 		return
-// 	}
-// 	rw.Header().Set("Content-Type", "application/json;charset=UTF-8")
-// 	rw.WriteHeader(http.StatusOK)
-// 	rw.Write(b)
-// }
-
-// func Welcome(rw http.ResponseWriter, request *http.Request) {
-// 	rw.Write([]byte("Welcome ! type :3000/Menu to seen Menu"))
-// }
-
-
-func main() {
-
-	// r := mux.NewRouter()
-	// r.HandleFunc("/", Welcome).Methods("GET")
-	// r.HandleFunc("/Menu", MenuPage).Methods("GET")
-
-	// http.ListenAndServe(":3000", r)
 	conn, err:= sqlx.Connect("mysql","sa:qwer!1234@tcp(127.0.0.1:3306)/orderservice")
 	if err != nil{
 		log.Fatalln(err)
 	}
-
+	defer conn.Close()
 	Menu:=[]Menu{}
 	conn.Select(&Menu,"select * from menu")
-	log.Println("Menu...")
-	log.Println(Menu)
+
+	b, err := json.Marshal(Menu)
+
+	if err != nil {
+		return
+	}
+	rw.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(b)
+}
+
+func AddMenu(rw http.ResponseWriter, request *http.Request) {
+	// rw.Write([]byte("Welcome ! type :3000/Menu to seen Menu"))
+
+
+	data := make(map[string]interface{})
+	decoder := json.NewDecoder(request.Body)
+	err := decoder.Decode(&data)
+	if err != nil {
+		return
+	}
+	// c := request.Body
+	// b, err := json.Marshal(c)
+	// b, err := json.Marshal(decoder)
+
+
+	// rw.Write([]byte(decoder))
+
+}
+
+
+func main() {
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", MenuPage).Methods("GET")
+	r.HandleFunc("/AddMenu",AddMenu).Methods("POST")
+	http.ListenAndServe(":3000", r)
+	
 }
